44b848abb645ef4d98f5904a1555be47d1671308
Amazonian32 <ynitsedekralc@gmail.com>
2020-01-17 20:39:36 -0700

    I completely re-wrote the js code. The questions are a simple array, the answers are an array; I made for loops in the functions to render the questions and answers. I am starting on the button click function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,57 @@
 
-var quizQuestions = [
-    { q: "The sky is blue.", a: "t"  },
-    { q: "There are 365 days in a year.", a: "t" },
-    { q: "There are 42 ounces in a pound.", a: "f" },
-    { q: "The Declaration of Independence was created in 1745.", a: "f" },
-    { q: "Bananas are vegetables.", a: "f" }
-]
+// var quizQuestions = [
+//     { q: "The sky is blue.", a: "t" },
+//     { q: "There are 365 days in a year.", a: "t" },
+//     { q: "There are 42 ounces in a pound.", a: "f" },
+//     { q: "The Declaration of Independence was created in 1745.", a: "f" },
+//     { q: "Bananas are vegetables.", a: "f" }
+// ]
+var questions = ["The sky is blue.", "There are 365 days in a year.", "There are 42 ounces in a pound.", "The Declaration of Independence was created in 1745.", "Bananas are vegetables."]
 
-var quizAnswers = [
-    {a: "t", b: "f", c: "f", d: "f"}
-];
+// var quizAnswers = [
+//     { a: "t", b: "f", c: "f", d: "f" }
+// ];
+var answers = ["t", "f"]
 
 let index = 0
 
-let choices = quizAnswers.length[index]
-
-for (let i = 0; i < quizQuestions.length; i++) {
-    console.log(quizQuestions[i]);
-    
-}
-for (let j = 0; j < quizAnswers.length; j++) {
-    console.log(quizAnswers[j]);
-
-    
-}
+// let choices = quizAnswers.length[index]
 
 $(document).ready(function () {
 
     function askQuestion() {
-        if (index <= (quizQuestions.length - 1)) {
-            var currentQ = $("<h2>").text(quizQuestions[index].q)
-            console.log(currentQ[0], quizQuestions[index].a);
-            $("#question").prepend(currentQ);
-        }else{
+
+        if (index <= (questions.length - 1)) {
+            for (let i = 0; i < questions.length; i++) {
+                var currentQ = $("<h2>")
+                currentQ.text(questions[i])
+                console.log(questions[i]);
+            }
+            $("#question").append(currentQ);
+
+        } else {
             console.log("GAME OVER");
             $("#timer").empty();
             $("#question").empty();
         }
-        function multipleChoice(){
+        function multipleChoice() {
+            for (let i = 0; i < answers.length; i++) {
+                console.log(answers[i]);
+                var choices = $("<button>")
+                choices.text(answers[i]).attr("id", answers[i]);
+                $("#choices").append(choices);
+            }
 
         }
 
-       
+        multipleChoice();
     }
-    
+
     askQuestion();
-})
+
+    $("button").on("click", function (event) {
+        console.log("clicked: " + $(this).attr("id"));
+
+    })
+
+});
